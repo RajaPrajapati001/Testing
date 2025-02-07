83725657cce3fb3d706667dc79223ed746114225
RajaPrajapati001 <rajaprajapati3454@gmail.com>
2025-02-07 10:52:36 +0530

    new changes

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ console.log("my commit");
 console.log("krishna bhaiya");
 console.log("krishna please do new commit");
 console.log("hello mere bhai");
-console.log("i have changed some thing");
+console.log("i have changed some thing is related to master");
